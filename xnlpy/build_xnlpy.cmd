4602740cc2deeb6520d16ed041919cf7cc600920
gilbertomourao <gilbertojos.mourao@gmail.com>
2021-05-15 18:22:05 -0300

    coming back

--- xnlpy/build_xnlpy.cmd
+++ xnlpy/build_xnlpy.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+for /f "tokens=2 delims=:." %%x in ('chcp') do set cp=%%x
+chcp 1252>nul
+
 rem set build_dir=%CD%
 
 rem finding python
@@ -30,12 +33,12 @@ echo.
 rem creating the folder xnlpy
 echo Checking if xnlpy already exists...
 rem cd %python_path%
-if exist %python_path%\xnlpy (
+if exist "%python_path%\xnlpy" (
 	echo Deleting the file...
-	rmdir /q /s %python_path%\xnlpy
+	rmdir /q /s "%python_path%\xnlpy"
 )
 echo Creating xnlpy...
-mkdir %python_path%\xnlpy
+mkdir "%python_path%\xnlpy"
 
 echo.
 echo ---------------------------------------------------------------------
@@ -47,11 +50,15 @@ rem copying data
 echo Moving files to the Python directory...
 rem cd %build_dir%\build\lib.win32-3.8
 echo Moving the compiled module...
-copy build\lib.win32-3.8\xnlpy* "%python_path%\xnlpy"
+copy build\lib.win-amd64-3.8\xnlpy* "%python_path%\xnlpy"
 rem cd ../..
 echo Moving __init__.py...
 copy __init__.py "%python_path%\xnlpy"
 
+cd "%python_path%\xnlpy"
+
 echo.
 echo ---------------------------------------------------------------------
 echo Done.
+
+chcp %cp%>nul
